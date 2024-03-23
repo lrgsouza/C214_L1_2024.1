85854f22c846b7ac6d7af1c8753043bee527f2f4
lrgsouza <lucas_souza@outlook.com>
2024-03-23 17:28:14 -0300

    Prepare branch to currency converter app

--- src/services/interfaces/ToDoList.ts
+++
@@ -1,8 +0,0 @@
-import { Task, UpdateTask } from '../../models/Task'
-
-export interface TodoListInt {
-    add: (task: Task) => string | Error
-    getTasks: () => Task [] | string | unknown
-    updateTask: (task: UpdateTask) => void
-    removeTask: (index: number) => void
-}
